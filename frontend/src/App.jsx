0cc6fa13dde2a5b166693db88a6c3a269318102a
sbansal05 <siddhi.bansal0504@gmail.com>
2026-01-25 21:39:50 +0530

    Added frontend signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Signup } from "./pages/Signup"
+import { Signin } from "./pages/Signin"
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
